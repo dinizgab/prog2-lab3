e8d1f908a267a3542c1c41b72a22d455b1b63f06
Gabriel <gabrielpacirnediniz@gmail.com>
2022-06-13 17:16:22 -0300

    Upando arquivos

---
+++ src/agenda/Agenda.java
@@ -0,0 +1,99 @@
+package agenda;
+
+/**
+ * Uma agenda que mantém uma lista de contatos com posições. Podem existir 100
+ * contatos.
+ * 
+ * @author nazareno
+ *
+ */
+public class Agenda {
+
+  private static final int TAMANHO_AGENDA = 100;
+
+  private static final int TAMANHO_FAVORITOS = 10;
+
+  private Contato[] contatos;
+
+  private Contato[] favoritos;
+
+  /**
+   * Cria uma agenda.
+   */
+  public Agenda() {
+    this.contatos = new Contato[TAMANHO_AGENDA];
+    this.favoritos = new Contato[TAMANHO_FAVORITOS];
+  }
+
+  /**
+   * Acessa a lista de contatos mantida.
+   * 
+   * @return O array de contatos.
+   */
+  public Contato[] getContatos() {
+    return this.contatos.clone();
+  }
+
+  public Contato[] getFavoritos() {
+    return this.favoritos.clone();
+  }
+
+  public void cadastraFavorito(int posicaoContato, int posicaoFavorito) {
+    Contato contatoFavoritado = this.contatos[posicaoContato];
+    Contato contatoFavorito = this.favoritos[posicaoFavorito];
+
+    if(contatoFavorito != null) {
+      contatoFavoritado.setFavorito();
+      contatoFavorito.setFavorito();
+    } else {
+      contatoFavoritado.setFavorito();
+    }
+    
+    // TODO - Achar uma maneira de isso parar de retornar ao valor null dentro do array de favoritos
+    contatoFavorito = contatoFavoritado;  
+  }
+
+  public boolean validaFavorito(int posicaoContato) {
+    for (int i = 0; i < favoritos.length; i++) {
+      if (contatos[posicaoContato].equals(favoritos[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Acessa os dados de um contato específico.
+   * 
+   * @param posicao Posição do contato na agenda.
+   * @return Dados do contato. Null se não há contato na posição.
+   */
+  public Contato getContato(int posicao) {
+    return contatos[posicao];
+  }
+
+  /**
+   * Cadastra um contato em uma posição. Um cadastro em uma posição que já existe
+   * sobrescreve o anterior.
+   * 
+   * @param posicao   Posição do contato.
+   * @param nome      Nome do contato.
+   * @param sobrenome Sobrenome do contato.
+   * @param telefone  Telefone do contato.
+   */
+  public void cadastraContato(int posicao, String nome, String sobrenome, String telefone) {
+    Contato novoContato = new Contato(nome, sobrenome, telefone);
+    this.contatos[posicao] = novoContato;
+  }
+
+  public void cadastraTag(String[] contatosDeCadastro, String tag, int posicaoTag) {
+    for (String contato : contatosDeCadastro) {
+      int indexContato = Integer.parseInt(contato);
+      
+      // TODO - Acontecendo o mesmo erro dos favoritos
+      
+      String[] tags = this.contatos[indexContato].getTags();
+      tags[posicaoTag] = tag;
+    }
+  }
+}
---
+++ src/agenda/Contato.java
@@ -0,0 +1,92 @@
+package agenda;
+
+public class Contato {
+  private static final int TOTAL_TAGS = 5;
+  private String nome;
+  private String sobrenome;
+  private String telefone;
+  private boolean favorito;
+  private String[] tags;
+
+  Contato(String nome, String sobrenome, String telefone) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.telefone = telefone;
+    this.favorito = false;
+    this.tags = new String[TOTAL_TAGS];
+  }
+
+  public String getNomeCompleto() {
+    return this.nome + " " + this.sobrenome;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    String nomeContato = this.getNomeCompleto();
+
+    if (obj == null)
+      return false;
+    if (obj.getClass() != this.getClass())
+      return false;
+
+    Contato objContato = (Contato) obj;
+    return nomeContato.equals(objContato.getNomeCompleto());
+  }
+
+  @Override
+  public String toString() {
+    String tags = formataTags();
+    return this.nome + " " + this.sobrenome + "\n" + this.telefone + "\n" + tags;
+  }
+
+  public void setFavorito() {
+    this.favorito = !this.favorito;
+  }
+
+  public boolean getFavorito() {
+    return this.favorito;
+  }
+
+  public String[] getTags() {
+    return this.tags.clone();
+  }
+
+  private String formataTags() {
+    String tagsFormatadas = "";
+    for (String tag : tags) {
+      
+      if (tag != null) {
+        tagsFormatadas += tag;
+      }
+    }
+    return tagsFormatadas;
+  }
+
+  public static boolean validaPosicao(int posicao) {
+    if (0 > posicao || posicao > 100) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public static boolean validaCampoVazio(String campo) {
+    if (campo.equals("") || campo == null) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public static boolean validaRepeticao(String nome, String sobrenome, Agenda agenda) {
+    Contato[] listaDeContatos = agenda.getContatos();
+    for (int i = 0; i < listaDeContatos.length; i++) {
+      String nomeCadastro = nome + " " + sobrenome;
+
+      if (listaDeContatos[i] != null && nomeCadastro.equals(listaDeContatos[i].getNomeCompleto())) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
